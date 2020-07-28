bab733455f6ef4657da84010af5ade44efbe127a
David Langheiter <david@langheiter.com>
2020-07-28 15:50:21 +0200

    Moved connected display to own class

---
+++ lib/connectedScreen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'consts.dart';
+import 'switches/switches.dart';
+import 'train/demoTrainControl.dart';
+import 'train/trainFunctions.dart';
+
+class ConnectedScreen extends StatefulWidget {
+  final String ecosName;
+  
+  ConnectedScreen(this.ecosName);
+  
+  @override
+  State<StatefulWidget> createState() => _ConnectedScreenState();
+}
+
+class _ConnectedScreenState extends State<ConnectedScreen> with SingleTickerProviderStateMixin {
+  static List<Tab> _tabs = <Tab>[
+    Tab(text: 'Trains', icon: Icon(Icons.train)),
+    Tab(text: 'Switches', icon: Icon(Icons.call_split)),
+  ];
+
+  TabController _tabController;
+  
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: _tabs.length);
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.ecosName),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: _tabs,
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Expanded(
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  TrainFunctions(),
+                  Switches(),
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  border: Border(
+                      top: BorderSide(
+                          color: Theme.of(context).highlightColor, width: 2))),
+              child: DemoTrainControl(
+                number: 24,
+                protocol: "DCC 128",
+                name: "Schnelli",
+                speed: 28,
+                maxSpeed: 128,
+                direction: Direction.forward,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecos_control/connectedScreen.dart';
 import 'package:ecos_control/switches/switches.dart';
 
 import 'consts.dart';
@@ -49,68 +50,9 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage>
-    with SingleTickerProviderStateMixin {
-  final List<Tab> _tabs = <Tab>[
-    Tab(text: 'Trains', icon: Icon(Icons.train)),
-    Tab(text: 'Switches', icon: Icon(Icons.call_split)),
-  ];
-
-  TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(vsync: this, length: _tabs.length);
-  }
-
+class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        bottom: TabBar(
-          controller: _tabController,
-          tabs: _tabs,
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            Expanded(
-              child: TabBarView(
-                controller: _tabController,
-                children: [
-                  TrainFunctions(),
-                  Switches(),
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  border: Border(
-                      top: BorderSide(
-                          color: Theme.of(context).highlightColor, width: 2))),
-              child: DemoTrainControl(
-                number: 24,
-                protocol: "DCC 128",
-                name: "Schnelli",
-                speed: 28,
-                maxSpeed: 128,
-                direction: Direction.forward,
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
+    return ConnectedScreen("Kirchberg"); 
   }
 }
